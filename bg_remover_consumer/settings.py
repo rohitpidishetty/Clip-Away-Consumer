fb47927182138550146965a704345442f11837ae
Er. P. Rohit V. Acharya <61237636+rohitpidishetty@users.noreply.github.com>
2026-03-07 09:17:09 -0600

    Add CORS support and update Django settings

--- bg_remover_consumer/settings.py
+++ bg_remover_consumer/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "clip_away",
+    "corsheaders",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
@@ -126,4 +127,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
 
